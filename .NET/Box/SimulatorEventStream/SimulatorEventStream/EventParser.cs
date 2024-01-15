a8a28e0e881fd756cab1457bf2bbd087dded323a
Jakub Woczko <jakub.wolczko@outlook.com>
2024-01-15 20:37:33 +0100

    Solution for Box company recriutment task

---
+++ .NET/Box/SimulatorEventStream/SimulatorEventStream/EventParser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace SimulatorEventStream;
+public class EventParser
+{
+    public static List<EventRecord> ParseCSV(string filePath)
+    {
+        List<EventRecord> records = new();
+
+        using (TextFieldParser parser = new TextFieldParser(filePath))
+        {
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            // Skip header
+            parser.ReadLine();
+
+            while (!parser.EndOfData)
+            {
+                string[] fields = parser.ReadFields();
+                if(fields ==  null || fields.Length == 0)
+                    continue;
+
+                if (fields.Length == 4)
+                {
+                    EventRecord record = new EventRecord
+                    {
+                        Identifier = Convert.ToInt64(fields[0]),
+                        EventTs = Convert.ToInt64(fields[1]),
+                        Payload = fields[2],
+                        Checksum = Convert.ToInt32(fields[3])
+                    };
+
+                    records.Add(record);
+                }
+            }
+        }
+
+        return records;
+    }
+}
---
+++ .NET/Box/SimulatorEventStream/SimulatorEventStream/EventRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace SimulatorEventStream;
+public class EventRecord
+{
+    public long Identifier { get; set; }
+    public long EventTs { get; set; }
+    public string Payload { get; set; }
+    public int Checksum { get; set; }
+}
