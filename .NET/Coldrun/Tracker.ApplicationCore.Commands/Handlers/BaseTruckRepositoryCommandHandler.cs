0872be7e73e404da7b1a9b983f4622a458e0abf6
Jakub Woczko <jakub.wolczko@outlook.com>
2024-01-31 21:41:44 +0100

    Coldrum Task part 5 some reafactoring

---
+++ .NET/Coldrun/Tracker.ApplicationCore.Commands/Handlers/BaseTruckRepositoryCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Tracker.Core;
+using Tracker.Domain;
+using Tracker.Repository;
+
+namespace Tracker.ApplicationCore.Commands.Handlers;
+public abstract class BaseTruckRepositoryCommandHandler
+{
+    protected readonly ITruckRepository _repository;
+
+    protected BaseTruckRepositoryCommandHandler(ITruckRepository repository)
+    {
+        _repository = repository;
+    }    
+
+    protected Task UpdateTruck(Task<Truck> task)
+    {
+        if (task.Result == null)
+            return Task.CompletedTask;
+
+        return _repository.UpdateTruckAsync(task.Result);
+    }
+}
--- .NET/Coldrun/Tracker.ApplicationCore.Commands/Handlers/SetNextStatusCommandHandler.cs
+++ .NET/Coldrun/Tracker.ApplicationCore.Commands/Handlers/SetNextStatusCommandHandler.cs
@@ -5,21 +5,30 @@ using Tracker.Repository;
 
 namespace Tracker.ApplicationCore.Commands.Handlers;
 
-public class SetNextStatusCommandHandler : IRequestHandler<SetNextStatusCommand>
+public class SetNextStatusCommandHandler : BaseTruckRepositoryCommandHandler, 
+    IRequestHandler<SetNextStatusCommand>
 {
-    private readonly ITruckRepository _repository;
-    private readonly ITruckState _truckState;
+    protected readonly ITruckState _truckState;
 
     public SetNextStatusCommandHandler(ITruckState truckState, ITruckRepository truckRepository)
+        :base (truckRepository)
     {
-        _repository = truckRepository;
         _truckState = truckState;
     }
 
-    public async Task Handle(SetNextStatusCommand request, CancellationToken cancellationToken)
+    public Task Handle(SetNextStatusCommand request, CancellationToken cancellationToken)
     {
-        Truck truckToUpodate = await _repository.GetTruck(request.Code).ConfigureAwait(false);
-        _truckState.SetNextState(truckToUpodate);
-        _ = _repository.UpdateTruckAsync(truckToUpodate);
+        return _repository.GetTruck(request.Code)
+            .ContinueWith(SetNextState)
+            .ContinueWith(UpdateTruck);
+    }
+
+    protected Truck SetNextState(Task<Truck> task)
+    {
+        if (task.Result == null)
+            return null;
+
+        _truckState.SetNextState(task.Result);
+        return task.Result;
     }
 }
